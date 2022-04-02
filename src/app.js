a7995abb4558f135f8f67eb4e3d9db0bb75fc188
Amanpreet <abinepal@my.bcit.ca>
2022-04-01 21:14:41 -0700

    added styling for part 3 of lab

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ const originalCont = document.querySelector(".original-container")
 const copyCont = document.querySelector(".copy-container")
 const deleteElem = document.querySelector(".copies")
 
+
 function copySection(evt) {
         const copyElem = evt.target.cloneNode(true)    
         copyCont.insertAdjacentElement("afterbegin", copyElem)
 }
 
-function deleteSection(evt) {
+function deleteSection() {
         while (copyCont.hasChildNodes()) {
                 copyCont.firstChild.remove()
-        }
-        
+        }     
 }
 
 originalCont.addEventListener("click", copySection)
